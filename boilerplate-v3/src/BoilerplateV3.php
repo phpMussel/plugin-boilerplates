547e3cf95da2434ecbbcac22675224d491a37202
Caleb Mazalevskis <maikuolan@gmail.com>
2020-07-19 23:11:18 +0800

    Add phpMussel v3 plugin boilerplate code.
    (Documentation and instructions still pending. I'll get to it when I can).

---
+++ boilerplate-v3/src/BoilerplateV3.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Boilerplate plugin class for phpMussel v3.
+ */
+
+namespace phpMussel\BoilerplateV3;
+
+class BoilerplateV3
+{
+    /**
+     * @var \phpMussel\Core\Loader The instantiated loader object.
+     */
+    private $Loader;
+
+    /**
+     * @var string The path to the linker asset files.
+     */
+    private $AssetsPath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR;
+
+    /**
+     * @var string The path to the linker L10N files.
+     */
+    private $L10NPath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'l10n' . DIRECTORY_SEPARATOR;
+
+    /**
+     * Construct the boilerplate instance.
+     *
+     * @param \phpMussel\Core\Loader $Loader The instantiated loader object, passed by reference.
+     */
+    public function __construct(\phpMussel\Core\Loader &$Loader)
+    {
+        /** Link the loader to this instance. */
+        $this->Loader = &$Loader;
+
+        /** Load configuration defaults and perform fallbacks. */
+        if (
+            is_readable($this->AssetsPath . 'config.yml') &&
+            $Configuration = $this->Loader->readFile($this->AssetsPath . 'config.yml')
+        ) {
+            $Defaults = [];
+            $this->Loader->YAML->process($Configuration, $Defaults);
+            if (isset($Defaults)) {
+                $this->Loader->fallback($Defaults);
+                $this->Loader->ConfigurationDefaults = array_merge_recursive($this->Loader->ConfigurationDefaults, $Defaults);
+            }
+        }
+
+        /** Load L10N data. */
+        $this->Loader->loadL10N($this->L10NPath);
+    }
+
+    /**
+     * Parses the ScanResultsText through ROT13 after scanning finishes.
+     *
+     * Totally useless, really. Pointless to use in production. But, useful as
+     * a super simplistic demonstration of how to write phpMussel v3 plugins.
+     */
+    public function __invoke(string $NotUsed): bool
+    {
+        /** Guard. */
+        if (empty($this->Loader->ScanResultsText)) {
+        }
+
+        /** Iterate through the ScanResultsText. */
+        foreach ($this->Loader->ScanResultsText as &$Results) {
+            $Results = str_rot13($Results);
+        }
+
+        /** Exit. */
+        return true;
+    }
+}
